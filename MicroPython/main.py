8a6ae56c5a9e83c2a7dd0b439d6b9c7198c7c812
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2023-10-02 02:23:37 +0000

    HEXIFYING MAIN.PY TO HEX!!

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,21 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Julianne Leblanc-Peltier
+Created on: Sep 2023
+This module turns an LED on a breadboard on and off.
 """
 
 from microbit import *
 
 
-display.scroll("Hello, World!")
+# setup
+display.clear()
+pin16.write_digital(0)
+display.show(Image.HAPPY)
+
+while True:
+    if button_a.is_pressed():
+        pin16.write_digital(1)
+        display.show(Image.YES)
+    if button_b.is_pressed():
+        pin16.write_digital(0)
+        display.show(Image.NO)
